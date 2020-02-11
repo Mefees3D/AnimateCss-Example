91fe12eaaa1a37d58927da325e3cbf5675ed9a4f
Guillermo Fernández <gfernandezestrella@gmail.com>
2020-02-11 00:02:31 -0600

    GoLive
    Example

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    
+    var options = {
+        animateThreshold: 150, //pixeles de aparicion
+        scrollPollInterval: 20 //tiempo de aparicion
+    }
+    $('.aniview').AniView(options);
+    
+});
